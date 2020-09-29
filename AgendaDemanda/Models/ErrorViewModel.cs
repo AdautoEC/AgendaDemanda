55bc6518fe78d895efb56208ff2f335d71f0f3fb
Adauto Neto <adauto.ec@gmail.com>
2020-09-29 13:36:58 -0400

    .

---
+++ AgendaDemanda/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AgendaDemanda.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
